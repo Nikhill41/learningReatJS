0d3a464fda95f7cc53fcb9c4c74384e29d2be273
Nikhil Pal <nikhilpal630622@mail.com>
2025-12-23 15:02:54 +0530

    tailwind include

--- Tailwind/src/App.jsx
+++ Tailwind/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>App</div>
+    <div  className='h-screen w-full bg-gray-900'>
+      App
+    </div>
   )
 }
 
